69ada341f7122efd5bab2f2800f50960093359ab
reggyshicky <reginahshikanda@gmail.com>
2023-11-29 15:39:34 +0300

    Promise.all returns a single promise from an array of promises as input

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignUp() {
+  const p1 = uploadPhoto();
+  const p2 = createUser();
+
+  return Promise.all([p1, p2])
+    .then((data) => {
+      const result = Object.assign(...data);
+      console.log(`${result.body} ${result.firstName} ${result.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
