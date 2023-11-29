f59d11ae0bc777a40d5b4ba53497a86d198185d9
reggyshicky <reginahshikanda@gmail.com>
2023-11-29 17:59:32 +0300

    return a promise that rejects and returns the Error message , new Error() function has to be used

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,21 @@
-import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignUp(firstName, lastName, fileName) {
-  const p1 = uploadPhoto(firstName, lastName);
-  const p2 = signUpUser(fileName);
-  const data = await Promise.allSettled([p1, p2])
-    .then((results) => results.map((result) => {
-      if (result.status !== 'fulfilled') {
-        return {
-          status: 'rejected',
-          value: result.reason.toString(),
-        };
-      }
-      return result;
-    }));
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const p1 = signUpUser(firstName, lastName);
+  const p2 = uploadPhoto(fileName);
+
+  const data = await Promise.allSettled([p1, p2]).then((results) => results.map((result) => {
+    if (result.status !== 'fulfilled') {
+      return {
+        status: 'rejected',
+        value: result.reason.toString(),
+      };
+    }
+    return result;
+  }));
 
   return Promise.resolve(data);
 }
+
+export default handleProfileSignup;
