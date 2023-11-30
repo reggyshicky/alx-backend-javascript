4abfcb263b961287161b06135377d359db315960
reggyshicky <reginahshikanda@gmail.com>
2023-12-01 00:54:23 +0300

    symbol

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -2,14 +2,10 @@ export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
-    this.color = color;
-  }
-
-  static get [Symbol.species]() {
-    return this;
+    this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor[Symbol.species]();
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
