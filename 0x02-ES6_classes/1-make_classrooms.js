d26b540c58d95d2ee57072b6764de4f893ca8442
reggyshicky <reginahshikanda@gmail.com>
2023-11-30 17:51:50 +0300

    created three instance with different students size

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const instance = new ClassRoom(19);
-  const instance1 = new ClassRoom(20);
-  const instance2 = new ClassRoom(24);
-
-  return [
-    `ClassRoom { _maxStudentsSize: ${instance._maxStudentsSize}}`,
-    `ClassRoom { _maxStudentsSize: ${instance1._maxStudentsSize}}`,
-    `ClassRoom { _maxStudentsSize: ${instance2._maxStudentsSize}}`,
-  ];
+  const arr = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return arr;
 }
