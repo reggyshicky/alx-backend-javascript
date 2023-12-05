e435c09ccb6d2ea7fc71e7dad95db407a92e78ab
reggyshicky <reginahshikanda@gmail.com>
2023-12-05 09:47:02 +0300

    weak maps

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+export const weakMap = new WeakMap();
+/*
+weak array
+*/
+export default function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    const endpointdata = weakMap.get(endpoint);
+    if (endpointdata >= 4) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, endpointdata + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
