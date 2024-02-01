ded20b82bb1c328becd5ea90183cd2b788a32204
reggyshicky <reginahshikanda@gmail.com>
2024-02-01 22:21:12 +0300

    payment test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const getPaymentTokenFromAPI = require('./6-payment_token');
+onst getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
   it('getPaymentTokenFromAPI(success), where success == true', (done) => {
